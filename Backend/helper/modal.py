627a15ff16d18fec1f7bfc86ac40b4e0ff02a6bf
keshav6606 <keshav6606@users.noreply.github.com>
2026-08-01 18:01:25 +0530

    fix: import Union in modal.py

--- Backend/helper/modal.py
+++ Backend/helper/modal.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from pydantic import BaseModel, Field, ValidationError
-from typing import List, Optional
+from typing import List, Optional, Union
 
 class QualityDetail(BaseModel):
     quality: str = Field(..., description="Quality of the video (e.g., 1080p, 720p)")
